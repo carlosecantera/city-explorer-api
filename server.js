6bed913fe660c264328ba1a824f780acde778256
Carlos De La Cantera <carlosecantera@yahoo.com>
2021-05-18 18:44:24 -0700

    lab 07

---
+++ server.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// const { response } = require('express');
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const PORT = process.env.PORT || 3030;
+
+app.use(cors());
+
+let weather = require('./Data/weather.json');
+
+
+app.get('/', (request, response) => {
+
+  response.send('Welcome to My Server');
+
+});
+
+app.get('/weather', (request, response) => {
+  
+  const data = [
+    'lat',
+    'lon',
+    'searchQuery'
+  ]
+  
+  // weather.find(findWeather)
+
+  // function findWeather(lat, data){
+  //   return lat === 'lat';
+  // }
+  // console.log(data);
+
+  response.json(data);
+  // weather.json(data);
+});
+
+app.listen(PORT, () => console.log('Server is running'));
