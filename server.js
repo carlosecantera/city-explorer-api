a38bf5d467c813e08e35cfb52fcd80bd00119f16
Carlos De La Cantera <carlosecantera@yahoo.com>
2021-05-19 19:50:47 -0700

    finished server

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 'use strict';
 
 // const { response } = require('express');
-
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3030;
 
+
+
+
 app.use(cors());
 
-let weather = require('./Data/weather.json');
+const weather = require('./Data/weather.json');
 
 
 app.get('/', (request, response) => {
@@ -18,23 +21,25 @@ app.get('/', (request, response) => {
 
 });
 
+// Create an API endpoint of `/weather` that processes a `GET` request that contains `lat`, `lon` and `searchQuery` information.
+
 app.get('/weather', (request, response) => {
+  let lat = request.query.lat;
+  let lon = request.query.lom;
+  let searchQuery = request.query.searchQuery;
   
-  const data = [
-    'lat',
-    'lon',
-    'searchQuery'
-  ]
-  
-  // weather.find(findWeather)
+  // Use the .find() method to discover which city the `lat`, `lon` and `searchQuery` information belong to. If the user did not search for one of the three cities that we have information about (Seattle, Paris, or Amman), return an error.
 
-  // function findWeather(lat, data){
-  //   return lat === 'lat';
-  // }
-  // console.log(data);
-
-  response.json(data);
-  weather.json(data);
+  const city = weather.find(city => city.city_name.toLowerCase() == searchQuery.toLowerCase());
+  try{
+    const weatherArray = city.data.map(day => new Forcast(day));
+    response.status(200).send(weatherArray);
+    
+  } catch(error){
+    response.status(400).send('City Not Found');
+  }
 });
+
+// To do create constructor function for forcast (day) date description
 
 app.listen(PORT, () => console.log('Server is running'));
